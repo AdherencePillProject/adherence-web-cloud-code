313b69ce7633c6eb5732c9725c2dd40811cd78cb
wmcalyj <mengchaowang@gmail.com>
2015-11-09 08:25:43 -0600

    Add logout link to home.html to allow users to logout

---
+++ public/js/logout.js
@@ -0,0 +1,20 @@
+// This is required for using Parse
+Parse.initialize("BDo39lSOtPuBwDfq0EBDgIjTzztIQE38Fuk03EcR", "ox76Y4RxB06A69JWAleRHSercHKomN2FVu61dfu3");
+
+function logout() {
+	var max, count;
+	count = 0;
+	max = 100;
+	var currentUser = Parse.User.current();
+	while (currentUser != null && count < max){
+		Parse.User.logOut();
+		count = count + 1;
+		currentUser = Parse.User.current();
+	}
+	if(currentUser){
+		alert("Failed to log you out. Please try to clear cache and try again.");
+	}else{
+		window.location.replace("login.html");
+	}
+	event.preventDefault();	
+}
