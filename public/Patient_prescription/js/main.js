d8c775d9cb6a236e9b9e1be9b840b52e80af4d36
JZhang <jzhangnu@gmail.com>
2016-02-24 15:28:03 -0600

    update

---
+++ public/Patient_prescription/js/main.js
@@ -0,0 +1,9 @@
+(function () { var body= $('body');
+	var side=$('nav.menu-side');
+    $('.menu-toggle').bind('click',function(){
+        body.toggleClass('menu-open');
+        side.toggleClass('menu-open');
+        return false;
+    })
+    })();
+
