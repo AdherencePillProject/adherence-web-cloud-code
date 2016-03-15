07f4475bb9971e123276d53c15b8aabbb0da88a0
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-03-15 00:31:07 -0500

    Edited angular controller to set active tabs based on route

--- public/js/angular-app.js
+++ public/js/angular-app.js
@@ -1,22 +1,12 @@
 var app = angular.module('adherence-partials', []);
 
 app.controller('MainController', function($scope) {
-  // $scope.msg = "Hello from main controller";
-  // $scope.hi= function(){console.log('hi');};
 });
 
-app.controller('NavController', function($scope) {
-	// $scope.id= $attrs.model;
-	// console.log($scope.id)
-	// angular.element('#' + $scope.id).addClass("active");
-	this.tab = 1;
-
-    this.setTab = function (tabId) {
-        this.tab = tabId;
-    };
-
-    this.isSet = function (tabId) {
-        return this.tab === tabId;
-    };
+app.controller('NavController', function($scope, $location) {
+  $scope.isActive = function (viewLocation) { 
+      
+      return $location.absUrl().indexOf(viewLocation) != -1;
+  };
 });
 
