e105c38314c61e75b1ef7827c1dde4c10a81808e
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-03-15 00:18:51 -0500

    Removed now unused navbar.js file

--- public/partials/navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-
-$('ul.nav li.dropdown').hover(function() {
-	$(this).find('.nav-hover').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  	$(this).find('.nav-hover').stop(true, true).delay(200).fadeOut(500);
-});// dashboard dropdown menu animation
