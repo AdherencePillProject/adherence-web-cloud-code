048b152d659788b195dcd84c58667602641144de
YeyiPeng <frankpeng7@hotmail.com>
2016-04-14 15:05:55 -0500

    Redesign the frame and data structure by Ounan

--- public/js/doctor_patientgraph.js
+++ public/js/doctor_patientgraph.js
@@ -7,7 +7,46 @@ var sameDiv = false;
 //keeps track of which prescription number we are on
 var prescriptionNum = 0;
 
+var color = ["rgba(255, 255, 255,1)", "rgba(197, 17, 98,1)", "rgba(0, 145, 234,1)", "rgba(30, 100, 234,1)"];
 
+var options = {
+  // fixes height of y-axis to be 24
+  scaleOverride: true,
+  scaleStartValue: 0,
+  scaleStepWidth: 2,
+  scaleSteps: 12,
+
+  ///Boolean - Whether grid lines are shown across the chart
+  scaleShowGridLines : true,
+  //String - Colour of the grid lines
+  scaleGridLineColor : "rgba(0,0,0,0.05)",
+  //Number - Width of the grid lines
+  scaleGridLineWidth : 1,
+  //Boolean - Whether to show horizontal lines (except X axis)
+  scaleShowHorizontalLines: true,
+  //Boolean - Whether to show vertical lines (except Y axis)
+  scaleShowVerticalLines: false,
+  //Boolean - Whether the line is curved between points
+  bezierCurve : false,
+  //Number - Tension of the bezier curve between points
+  bezierCurveTension : 0.3,
+  //Boolean - Whether to show a dot for each point
+  pointDot : true,
+  //Number - Radius of each point dot in pixels
+  pointDotRadius : 5,
+  //Number - Pixel width of point dot stroke
+  pointDotStrokeWidth : 1,
+  //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+  pointHitDetectionRadius : 20,
+  //Boolean - Whether to show a stroke for datasets
+  datasetStroke : true,
+  //Number - Pixel width of dataset stroke
+  datasetStrokeWidth : 2,
+  //Boolean - Whether to fill the dataset with a colour
+  datasetFill : true,
+  //String - A legend template
+  legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+};
 //toggleActive
 //parameters: div element
 //function: toggles the active class element for divs on the right
@@ -77,6 +116,19 @@ function getPatients() {
 //parameters: patient_name, count
 //function: creates div for name that shows up on the right
 
+var pills = ["pill1", "pill2", "pill3"];
+
+bool_arry = {};
+bool_arry["pill1"] = true;
+bool_arry["pill2"] = true;
+bool_arry["pill3"] = true;
+
+pill_time = {};
+pill_time[pills[0]] = [1,2,3,4,5,6,7];
+pill_time[pills[1]] = [4,2,1,3,5,4,7];
+pill_time[pills[2]] = [21,22,4,9,3,6,7];
+
+
 function createNameDiv(patient_name, count, pillName){	
 	//Add their names to div id="patient_names"
 	var pn = document.getElementById("patien_names");
@@ -85,7 +137,7 @@ function createNameDiv(patient_name, count, pillName){
 	newA.href = "#";
 	newA.id = "name" + count;
 	newA.onclick = function() { 
-		patient0Clicked(this, pillName);
+		patient0Clicked(this, patient_name);
 	}
 	var name = document.createElement("h4");
 	name.textContent = patient_name;
@@ -106,35 +158,125 @@ function createNameDiv(patient_name, count, pillName){
 	pn.appendChild(newA);
 }
 function createCheckBoxes (pillName) {
-	var cb_left = document.getElementById("checkbox-left");
-	var newDiv = document.createElement("div");
-	newDiv.className = "checkbox";
-	
-	var input = document.createElement("input");
-	input.type = "checkbox";
-	input.id = "checkbox" + "1";
-	
+	var cb_left = document.getElementById("checkbox-left");	
 
-	var cblabel = document.createElement("span");
-	cblabel.textContent = pillName;
+	// console.log("Children number", cb_left.childNodes.length)
 
-	var label = document.createElement("label");
-	label.appendChild(input);
-	label.appendChild(cblabel);
+	// while(cb_left.childNodes.length > 4){
+	// 	cb_left.removeChild(cb_left.childNodes[cb_left.childNodes.length - 1]);
+	// }
 
-	newDiv.appendChild(label);
-	cb_left.appendChild(newDiv);
+	// for(var i = 0; i < pillName.length; i++){
+		var newDiv = document.createElement("div");
+
+		newDiv.className = "checkbox";
+		var input = document.createElement("input");
+		input.type = "checkbox";
+		input.checked = true;
+		input.id = pillName;
+		input.onclick = function(){
+			onClickCB(this, input.id);
+		}
+
+		var cblabel = document.createElement("span");
+		cblabel.textContent = pillName;
+
+		var label = document.createElement("label");
+		label.appendChild(input);
+		label.appendChild(cblabel);
+
+		newDiv.appendChild(label);
+		cb_left.appendChild(newDiv);
+	// }
 
 }
+
+function onClickCB(ev, inputID){
+	console.log(inputID);
+	var cb = document.getElementById(inputID);
+	bool_arry[cb.id] = cb.checked;
+	drawGraph();
+	console.log(cb.id, cb.checked);
+	console.log("bool is", bool_arry[cb.id])
+}
+
+function selectAll(ev){
+	var cb = document.getElementById("selectallcheckboxes");
+	if(cb.checked){
+		var offAllCheckBox = document.getElementById("unselectallcheckboxes");
+		offAllCheckBox.checked = false;
+		for(var i = 0; i < pills.length; i++){
+			var check = document.getElementById(pills[i]);
+			check.checked = true;
+			bool_arry[check.id] = true;
+		}
+	}
+	drawGraph();
+}
+
+
+function unSelectAll(ev){
+	var cb = document.getElementById("unselectallcheckboxes");
+	if(cb.checked){
+		var allCheckBox = document.getElementById("selectallcheckboxes");
+		allCheckBox.checked = false;
+		for(var i = 0; i < pills.length; i++){
+			var check = document.getElementById(pills[i]);
+			check.checked = false;
+			bool_arry[check.id] = false;
+		}
+	}
+	drawGraph();
+}
+
+function drawGraph(){
+  	var ctx = document.getElementById("myChart").getContext("2d");
+	var time = [[1,1,1,1,1,1]];
+	for(var i = 0; i < pills.length; i++){
+		if(bool_arry[pills[i]])
+			time.push(pill_time[pills[i]]);
+	}
+
+	var datasets = [];
+  	for(i = 0; i < time.length; i++){
+    	datasets.push(          {
+        	    fillColor: "rgba(255,255,255,0)",
+            	strokeColor: color[i],
+              	pointColor: color[i],
+              	pointStrokeColor: "#fff",
+              	pointHighlightFill: "#fff",
+              	pointHighlightStroke: "rgba(220,220,220,1)",
+              	data: time[i]
+          })
+  	}
+  	var data = {
+    	labels: ["Mon", "TUE", "WED", "THUR", "FRI", "SAT", "SUN"],
+      	datasets
+    };
+  	//var dataNew = data;
+
+	var myLineChart = new Chart(ctx).Line(data, options);
+}
+
 function patient0Clicked(ev, pillName) {
 	toggleActive(ev);
-	// createCheckBoxes(pillName);
+	console.log(pillName);
+	drawGraph();
+	var cb_left = document.getElementById("checkbox-left");	
+
+	console.log("Children number", cb_left.childNodes.length)
+
+	while(cb_left.childNodes.length > 4)
+		cb_left.removeChild(cb_left.childNodes[cb_left.childNodes.length - 1]);
+	for(var i = 0; i < pills.length; i++)
+		createCheckBoxes(pills[i]);
 }
 
 
 //main()
 function main(){
 	getPatients();	
+
 }
 
 Parse.initialize("BDo39lSOtPuBwDfq0EBDgIjTzztIQE38Fuk03EcR", "ox76Y4RxB06A69JWAleRHSercHKomN2FVu61dfu3");
