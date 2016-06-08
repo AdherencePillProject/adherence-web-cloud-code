24aca8d9d78566053223ba86d09c244b7c7e48e4
Sarah(Baiyu) Yang <ybywmr@163.com>
2016-06-07 19:42:32 -0500

    fix patient prescripiton

--- public/patient/js/controllers/prescriptionCtrl.js
+++ public/patient/js/controllers/prescriptionCtrl.js
@@ -1,21 +1,22 @@
+
+'use strict';
 angular
 .module('app')
 .controller('prescriptionCtrl', ['$scope', '$rootScope', 'Patient', function($scope, $rootScope, Patient) {
-	$scope.checked = false;// if the patient have taken this pill: showing checked span
-	$scope.unchecked = !$scope.checked;// else showing unchecked;
+    $scope.checked = false;// if the patient have taken this pill: showing checked span
+    $scope.unchecked = !$scope.checked;// else showing unchecked;
     console.log($scope.unchecked);
 
-	// funtion: addZero
-	// add 0 to one digit numbers to modify the format of time
-	var addZero = function(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  };
+    // funtion: addZero
+    // add 0 to one digit numbers to modify the format of time
+    var addZero = function(i) {
+          if (i < 10) {
+              i = "0" + i;
+          }
+          return i;
+        };
 
 
-    $scope.prescriptions = [];
     $scope.month = "";
     $scope.date = "";
     var currentdate = new Date();
@@ -25,58 +26,69 @@ angular
     console.log($scope.month);
     console.log(currentdate.getDate());
     // get the abbrevation of day_of_week
-
+    
     var weekday = new Array(7);
-	    weekday[0] = "Sun";
-	    weekday[1] = "Mon";
-	    weekday[2] = "Tue";
-	    weekday[3] = "Wed";
-	    weekday[4] = "Thu";
-	    weekday[5] = "Fri";
-	    weekday[6] = "Sat";
+        weekday[0] = "Sun";
+        weekday[1] = "Mon";
+        weekday[2] = "Tue";
+        weekday[3] = "Wed";
+        weekday[4] = "Thu";
+        weekday[5] = "Fri";
+        weekday[6] = "Sat";
 
     $scope.day_of_week = weekday[currentdate.getDay()];
     console.log(currentdate.getDay());
     console.log($scope.day_of_week);
-    // pill_info
-    // include the information of pills a patient should take, please retrieve from database
-
-    var query = $rootScope.currentUser.relation('prescription').query();
-    // query.limit(5);
-    // query.descending('createdAt');
-    query.find({
-      success: function(res) {
-        $scope.$apply(function() {
-          $scope.prescriptions = res;
-        });
-      },
-      error: function(err) {
-        console.log('this didnt work')
-        console.log(err);
-      }
-    });
-
-
     console.log('current user is', $rootScope.currentUser.get('prescription'));
-    // pill_info
+    // pill_info 
     // include the information of pills a patient should take, please retrieve from database
+
+    $scope.pill_info = [
+        {
+            "index": "0",
+            "name" :"Antihistamine",
+            "dose" :"2",
+            "time":"09:30",
+            "intro":"Here is the detailed introduction to this pill and prescription inforamtion"
+        },
+
+        {
+            "index": "1",
+            "name" :"Sonata",
+            "dose" :"1",
+            "time":"11:30",
+            "intro":"Here is the detailed introduction to"
+        }
+    ];
+
+    // query.find({
+    //     var relation = User.relation('prescription');
+    // })
+
+    //function:show_detail
+    //click the the pill and show pill_detail
+  //   $scope.showDetaili = true;
+  //   $scope.show_detail = function(){
+        // $scope.showDetail = ! $scope.showDetail;
+  //   };
+
 
   $scope.open = function(item){
         if ($scope.isOpen(item)){
             $scope.opened = undefined;
         } else {
             $scope.opened = item;
-        }
+        }        
     };
-
+    
     $scope.isOpen = function(item){
         return $scope.opened === item;
     };
-
+    
     $scope.anyItemOpen = function() {
         return $scope.opened !== undefined;
     };
-
+    
     $scope.close = function() {
         $scope.opened = undefined;
     };
